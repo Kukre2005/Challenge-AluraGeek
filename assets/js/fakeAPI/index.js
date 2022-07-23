3ceb584988028cf994d33f5d841e9a1c04a23174
kukre2005 <ariasmanuel191@outlook.com>
2022-07-23 16:11:45 -0300

    Agregar archivos de proyecto.

---
+++ assets/js/fakeAPI/index.js
@@ -0,0 +1,214 @@
+
+export const lista = [{
+  titulo: "Star Wars",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "Taza Stormtrooper",
+      img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/814/563/products/okxdfg1-b5271c3b20669cc4d416370049322340-640-0.jpg",
+      preco: 900,
+      descricao: "Taza de porcelana de una cabeza de Stormtrooper",
+      alt: "Taza stormtrooper"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nome: "Taza Darth Vader",
+      img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/814/563/products/vader-ok1-84cc5fc020f640cf6616396850030595-640-0.png",
+      preco: 60.02,
+      descricao: "Taza de porcelana de Darth Vader",
+      alt: "Taza darth Vader"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nome: "Muñeco de Baby yoda",
+      img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/814/563/products/yostandok1-311c6333531a961e6e16192109607067-640-0.jpg",
+      preco: 60.03,
+      descricao: "Muñeco artesanal de Baby Yoda",
+      alt: "Baby Yoda"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nome: "Destapador del Halcón Milenario",
+      img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/814/563/products/s-l40011-3684f3401045b71cb216071187017564-640-0.jpg",
+      preco: 60.04,
+      descricao: "Destapador metalico con la figura del Halcón Milenario",
+      alt: "Halcón Milenario"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nome: "Caja Musical de Star Wars",
+      img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/814/563/products/51d57tkuovl-_ac_1-047042b101a8b5545b16299995803769-640-0.jpg",
+      preco: 60.05,
+      descricao: "Caja musical de madera de Star Wars",
+      alt: "caja musical"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nome: "Remera de Star wars",
+      img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/814/563/products/logo1-3d301a0ade9104483016020159760399-640-0.jpg",
+      preco: 60.06,
+      descricao: "Remera de color negra con estampado de Star Wars.",
+      alt: "Remera Star Wars."
+    }
+  },
+  {
+    infos: {
+      id: 7,
+      nome: "Malla de Star wars",
+      img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/814/563/products/1a53d03b-093c-44a9-bb92-3e1feca68abe1-f41ba3e246f3d239dc16415012210079-640-0.jpg",
+      preco: 60.07,
+      descricao: "Malla con motivo de Star Wars.",
+      alt: "Malla Stormtrooper"
+    }
+  }
+  ]
+},
+{
+  titulo: "Consola",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "Joystick PS5",
+      img: "https://www.cronista.com/files/image/443/443860/61f5f69b0ed5b_950_534!.jpg",
+      preco: 60.08,
+      descricao: "Joystick para la consola Playstation 5",
+      alt: "Joystick PS5"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nome: "Playstation 5",
+      img: "https://venturebeat.com/wp-content/uploads/2022/04/ps5-product-thumbnail-01-en-14sep21.png",
+      preco: 60.09,
+      descricao: "Consola de videojuegos Playstation 5",
+      alt: "PS5"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nome: "Consola Nintendo NES",
+      img: "https://www.giantfreakinrobot.com/wp-content/uploads/2022/04/NES-Console-Set-e1650662033341.jpeg",
+      preco: 60.10,
+      descricao: "Consola retro Nintendo NES",
+      alt: "NES"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nome: "Nintendo Switch",
+      img: "https://www.atajo.com.ar/images/00000HAD-S-KABAA65619HAD-S-KABAA-Consola-Nintendo-Switch-Neon-01.jpg",
+      preco: 60.11,
+      descricao: "Consola portatiel Nintendo SWITCH.",
+      alt: "Switch"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nome: "Xbox ONE S",
+      img: "https://media.redadn.es/imagenes/xbox_333656.jpg",
+      preco: 60.12,
+      descricao: "Consola de Viedojuegos XBOX ONE S (Color negra)",
+      alt: "Xbox ONE S"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nome: "Game Boy Color Pokemon",
+      img: "https://i.pinimg.com/originals/c6/e9/60/c6e960515b946a36c36b83e070c23fe4.jpg",
+      preco: 60.13,
+      descricao: "Game Boy Color conmemorativa a la serie pokemón, esta version es la llamada version YELLOW",
+      alt: "Pokemon Picachu"
+    }
+  },
+  {
+    infos: {
+      id: 7,
+      nome: "Consola Atari 5200",
+      img: "https://m.media-amazon.com/images/I/91sRIdo5tfL._AC_SL1500_.jpg",
+      preco: 60.14,
+      descricao: "Consola de videojuegos retro Atari 5200 en caja, contiene juegos y joysticks.",
+      alt: "Atari 5200"
+    }
+  }
+  ]
+},
+{
+  titulo: "Diversos",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "One piece",
+      img: "https://artesetra.com.br/wp-content/uploads/2021/01/20210102_one_piece_luffy.jpg",
+      preco: 60.15,
+      descricao: "One Piece es una serie que cuenta la historia de pirata Monkye D. Luffy, esta serie cuenta con mas de 1000 capitulos.",
+      alt: "One Piece"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nome: "One Punch-Man",
+      img: "https://m.media-amazon.com/images/I/619i1o5c2JL._SL1500_.jpg",
+      preco: 60.16,
+      descricao: "One Punch-Man cuenta la historia de una persona que mata monstruos por aburrimiento.",
+      alt: "OPM"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nome: "Nanatsu no Taizai",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsUq2WjD69n4Q5xiWxA93YMFf2LhKnlbtpgcZQO3iQXHtgNbC4M5r21s9SY1EHXPtdpBQ&usqp=CAU",
+      preco: 60.17,
+      descricao: "Nanatsu no Taizai (Los siete pecados capitales), es una serie de manga escrito e ilustrado por Nakaba Suzuki. Fue serializado en la revista Weekly Shōnen Magazine de Kodansha desde octubre de 2012 hasta marzo del 2020, con los capítulos completos en cuarenta volumenes de tankōbon.",
+      alt: "nanatsu no taizai"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nome: "Naruto",
+      img: "https://i.pinimg.com/originals/a7/5e/9e/a75e9e35a8ed1ccc1017db40438c60e9.jpg",
+      preco: 60.18,
+      descricao: "Naruto es una serie de manga escrita e ilustrada por Masashi Kishimoto, que cuenta la história de Naruto Uzumaki, un joven ninja que contanstemente procura el reconocimiento de su aldea, el tiene un camino a seguir y es el de convertirse en el hokage de su aldea.",
+      alt: "naruto"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nome: "Dragon Ball",
+      img: "http://pm1.narvii.com/7828/849557d07a9390fc03b546f24fca611a7ac51640r1-736-670v2_uhq.jpg",
+      preco: 60.19,
+      descricao: "Dragon Ball es una franquisia  japonesa creada por Akira Toriyama. Originalmente iniciada como una série de manga que fue escrita e ilustrada por Toriyama, tuvo todos sus capítulos serializados en la revista Weekly Shonen Jump de 1984 a 1995, con sus 519 capítulos compilados en 42 volumenes tankōbon y publicados por la editora Shueisha.",
+      alt: "goku ultra-instic"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nome: "Samurai-X",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT64owhBLUJa0enpJKiRdjiQN-gBcCghmY3Z3X07-_QKdgMZfMsc1JUyT1kEKFEAiQUOTY&usqp=CAU",
+      preco: 60.20,
+      descricao: "Samurai X es una serie de mangá creada por el artista Nobuhiro Watsuki y posteriormente adaptada en anime. La serie, ambientada en los primeros años de la Era Meiji en Japón, cuenta la história de Kenshin Himura un pacífico espadachin que promete nunca mas matar.",
+      alt: "Samurai com espada na mão"
+    }
+  },
+
+  ]
+}]
